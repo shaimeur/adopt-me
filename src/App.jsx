e3a161f67a524da32f3e1fb76b10e2fcf3364991
shaimeur <haimeursaad@gmail.com>
2023-05-17 10:51:21 +0100

    adding some more config ESLint for React

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Arcanine",
-//       animal: "Ino",
-//       breed: "Legendary",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Snorlax",
-//       animal: "Kuma",
-//       breed: "Legendary",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pikachu",
-//       animal: "souri",
-//       breed: "Shiny",
-//     }),
-//   ]);
-// };
-
 const App = () => {
   return (
     <div>
@@ -34,5 +13,4 @@ const App = () => {
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
-// root.render(React.createElement(App));
 root.render(<App />);
