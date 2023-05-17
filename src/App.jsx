6aef2a6dcc0afd8a9bb6aadb2a4b2dd9ca16c3f5
shaimeur <haimeursaad@gmail.com>
2023-05-17 10:23:06 +0100

    the start of using JSX instead of the creatElement syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import React from "react";
 import { createRoot } from "react-dom";
+import Pet from "./Pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Arcanine",
+//       animal: "Ino",
+//       breed: "Legendary",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Snorlax",
+//       animal: "Kuma",
+//       breed: "Legendary",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pikachu",
+//       animal: "souri",
+//       breed: "Shiny",
+//     }),
+//   ]);
+// };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Arcanine",
-      animal: "Ino",
-      breed: "Legendary",
-    }),
-    React.createElement(Pet, {
-      name: "Snorlax",
-      animal: "Kuma",
-      breed: "Legendary",
-    }),
-    React.createElement(Pet, {
-      name: "Pikachu",
-      animal: "souri",
-      breed: "Shiny",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name={"Arcanine"} animal={"Ino"} breed={"Legendary"} />
+      <Pet name={"Snorlax"} animal={"Kuma"} breed={"Legendary"} />
+      <Pet name={"Pikachu"} animal={"souri"} breed={"Shiny"} />
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+// root.render(React.createElement(App));
+root.render(<App />);
