e4a718382068c62cc79484a81d5ca06ad88414eb
shaimeur <haimeursaad@gmail.com>
2023-05-25 15:05:19 +0100

    creating a custom hook to generate a list of breeds when we request an animal we save it in the localCacheso if it loads once,it won't have to reload the same API call in the same session

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
   useEffect(() => {
     requestPets();
     // eslint-disable-next-line react-hooks/exhaustive-deps
